d46e39038bf638c90b582623079e420e221efe77
NiloyBarua <nbniloyofficial@gmail.com>
2024-11-25 15:42:49 +0600

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default ({ mode }) => 
-  defineConfig({
-    plugins: [react()],
-    base: mode === 'github' ? '/physixtry/' : '/',
-    build: {
-      sourcemap: true,
-    },
-  });
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Set to '/' for Vercel deployments
+  build: {
+    sourcemap: true,
+  },
+});
