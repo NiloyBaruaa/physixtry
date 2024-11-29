7bd9281e85f0cbdc96e9bd033a338422f0e242c0
NiloyBarua <nbniloyofficial@gmail.com>
2024-11-29 20:35:55 +0600

    Sonu

--- src/Components/Feature/Feature.jsx
+++ src/Components/Feature/Feature.jsx
@@ -11,7 +11,7 @@ const Feature = ({ feature }) => {
     }
     return (
         <section>
-            <div className="card card-compact bg-slate-800 text-white font-semibold w-96 border mx-auto h-full">
+            <div className="card card-compact bg-slate-800 text-white font-semibold w-6/7 border mx-auto h-full">
      
                 <div className="card-body text-center">
                     <h2 className="text-3xl p-4 mt-5 bg-white text-slate-800 rounded-md font-semibold">{feature_name}</h2>
