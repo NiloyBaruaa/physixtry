831beb962f1bb1116c102943328e95ef2a8af867
NiloyBarua <nbniloyofficial@gmail.com>
2024-12-05 19:03:17 +0600

    Bals

--- src/Components/Bio/Bio.jsx
+++ src/Components/Bio/Bio.jsx
@@ -1,17 +1,18 @@
 import ReactPlayer from "react-player/youtube";
+import { Link } from "react-router-dom";
 
 
 const Bio = ({ bio }) => {
-    const { subject_name, subject_url, subject_title } = bio;
+    const { subject_name, subject_url, subject_title, chapter_number ,exam_link} = bio;
     return (
         <div >
 
-            <div className="card bg-base-100 w-96 shadow-xl text-center font-bold text-2xl">
-                <ReactPlayer url={subject_url} height={270} width={380} controls volume></ReactPlayer>
+            <div className="card bg-blue-950 text-white shadow-xl text-center mx-3 font-bold text-2xl">
+                <div className="p-2 rounded-lg"><ReactPlayer url={subject_url} height={270} width={380} controls volume></ReactPlayer></div>
                 <div className="card-body">
-                    <h2 >{subject_name}</h2>
-                    <p>{subject_title}</p>
-             
+                 
+                    <p>Chapter {chapter_number} : {subject_title}</p>
+                    <p className="text-blue-600 text-base">Daily Test : <Link to={exam_link}>Chapter {chapter_number}</Link></p>
                 </div>
             </div>
 
